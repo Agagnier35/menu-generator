cf8caa8aa910e6ea364ebe54ed6c7aaf33bb9887
Felix Agagnier <felix_agagnier@hotmail.ca>
2020-12-21 14:02:22 -0500

    Init react-router (Infra should be done at this point)

--- react/src/page/index.tsx
+++ react/src/page/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import MainPage from './main-page';
+import SecondPage from './second-page';
 
 const App = () => {
-    return <div>This is form react my boi, YOLOOOOOOOO</div>;
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Main</Link>
+                        </li>
+                        <li>
+                            <Link to="/second">Second</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/second">
+                        <SecondPage />
+                    </Route>
+                    <Route path="/" exact>
+                        <MainPage />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
---
+++ react/src/page/second-page/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SecondPage = () => {
+    return <div>This is the second page</div>;
+};
+
+export default SecondPage;
